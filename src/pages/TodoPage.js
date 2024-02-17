7b7f487bcea1fce542e1f45920299d5edccb95e4
Alice Williams <alice.williams.tech@gmail.com>
2024-02-17 13:47:05 -0800

    todo page banner

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,11 +1,17 @@
 // src/pages/TodoPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import crimsonImg from '../public/crimson.png';
+import cwuImg from '../public/cwu.png';
 
 const TodoPage = () => {
     return (
         <div className="bg-light-mint min-h-screen flex flex-col">
-            <h1 className="text-2xl font-bold">Todo Page</h1>
+            <div className="bg-light-blue w-full flex justify-between p-4">
+                <img src={crimsonImg} alt="Crimson Code Logo" className="w-1/6"/>
+                <img src={cwuImg} alt="CWU School Logo" className="w-1/2"/>
+            </div>
+            <h1 className="text-3xl font-bold underline">Todo Page</h1>
             {/* Todo list functionality will go here */}
             <Link to="/" className="text-blue-500 hover:text-blue-800">Go to Home Page</Link>
         </div>
