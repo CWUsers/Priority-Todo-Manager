1754fd0f380890762096fd8cca400edf8679fae9
Alice Williams <alice.williams.tech@gmail.com>
2024-02-17 13:31:32 -0800

    home images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import crimsonImg from '../public/crimson.png';
+import cwuImg from '../public/cwu.png';
 
 function HomePage() {
   return (
     <div className="bg-light-mint min-h-screen flex flex-col justify-center items-center">
+      <img src={crimsonImg} alt="Crimson Code Logo" className="absolute top-0 left-0 mb-4 w-1/6"/>
+      <img src={cwuImg} alt="CWU School Logo" className="absolute top-0 right-0 mb-4 w-1/2"/>
       <h1 className="text-3xl font-bold underline">
         CWUsers Todo Application Home
       </h1>
