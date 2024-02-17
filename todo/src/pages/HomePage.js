028bfb4c7fb9bbb0b7f1625c03ac358f7f481335
Alice Williams <alice.williams.tech@gmail.com>
2024-02-17 11:25:02 -0800

    working react/tailwind/react-rrouter/redis/node

---
+++ todo/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// src/pages/HomePage.js
+function HomePage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      {/* Content here */}
+    </div>
+  );
+}
+
+export default HomePage;
+
---
+++ todo/src/pages/TodoPage.js
@@ -0,0 +1,14 @@
+// src/pages/TodoPage.js
+import React from 'react';
+
+const TodoPage = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold">Todo Page</h1>
+            {/* Todo list functionality will go here */}
+        </div>
+    );
+};
+
+export default TodoPage;
+
